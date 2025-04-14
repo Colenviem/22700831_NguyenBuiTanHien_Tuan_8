0f793191c20e1032e3bc9476e4cb25fcccf86098
Colenviem <hiennguyenbuitan@gmail.com>
2025-04-14 22:18:00 +0700

    Recipe Card commit

--- nguyenbuitanhien_22700831/src/App.jsx
+++ nguyenbuitanhien_22700831/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
+import RecipeCard from './components/RecipeCard'
 
 function App() {
 
   return (
     <>
       <Header/>
+      <RecipeCard />
     </>
   )
 }
